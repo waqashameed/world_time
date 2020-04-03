e09da3cd54668b8f8dc790c3e7597c369cd23b02
Waqas <m_waqas_hameed@hotmail.com>
2020-04-04 00:59:07 +0500

    Called world time api to get the time for Karachi using asynchronous code

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,30 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    Response response =
+        await get('http://worldtimeapi.org/api/timezone/Asia/Karachi');
+    Map data = jsonDecode(response.body);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'];
+    offset = offset.substring(1, 3);
+
+    //print(datetime);
+    //print(offset);
+
+    //Creating Datetime
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
