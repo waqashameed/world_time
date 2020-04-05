59ab0f9add04ea45e0c781b1fdc74e4874117802
Waqas <m_waqas_hameed@hotmail.com>
2020-04-05 20:22:36 +0500

    Formatted date and showed it in the Home screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,17 +13,46 @@ class _HomeState extends State<Home> {
     print(data);
 
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            FlatButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 120, 0.0, 0.0),
+            child: Column(
+              children: <Widget>[
+                RaisedButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text('Edit Location'),
+                  elevation: 2.0,
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
